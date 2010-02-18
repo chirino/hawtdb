81c48ee1631e8fe361049aaae3dd76d63cb0d602
Hiram Chirino <hiram@hiramchirino.com>
2010-02-18 13:44:06 -0500

    Forking https://svn.apache.org/repos/asf/activemq/sandbox/activemq-apollo/hawtdb to it's own project since this module has a big potential to be more re-usable.

---
+++ src/test/java/org/fusesource/hawtdb/internal/index/HashIndexBenchmark.java
@@ -0,0 +1,45 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fusesource.hawtdb.internal.index;
+
+import org.fusesource.hawtdb.api.HashIndexFactory;
+import org.fusesource.hawtdb.api.Index;
+import org.fusesource.hawtdb.api.Transaction;
+import org.fusesource.hawtdb.util.buffer.Buffer;
+import org.fusesource.hawtdb.util.marshaller.FixedBufferMarshaller;
+import org.fusesource.hawtdb.util.marshaller.LongMarshaller;
+
+
+/**
+ * 
+ * @author <a href="http://hiramchirino.com">Hiram Chirino</a>
+ */
+public class HashIndexBenchmark extends IndexBenchmark {
+
+    public HashIndexBenchmark() {
+        this.benchmark.setSamples(5);
+    }
+    
+    protected Index<Long, Buffer> createIndex(Transaction tx) {
+        HashIndexFactory<Long, Buffer> factory = new HashIndexFactory<Long, Buffer>();
+        factory.setKeyMarshaller(LongMarshaller.INSTANCE);
+        factory.setValueMarshaller(new FixedBufferMarshaller(DATA.length));
+        factory.setFixedCapacity(500);
+        return factory.create(tx, tx.allocator().alloc(1));
+    }
+    
+}
