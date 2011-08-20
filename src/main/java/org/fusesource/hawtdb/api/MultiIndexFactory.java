a725c4f6bf7f35f302d581ed469847c634ed9db9
Sergio Bossa <sergio.bossa@gmail.com>
2011-08-20 15:45:40 +0200

    Improved multi-index apis by avoiding passing the paged object multiple times.

--- src/main/java/org/fusesource/hawtdb/api/MultiIndexFactory.java
+++ src/main/java/org/fusesource/hawtdb/api/MultiIndexFactory.java
@@ -17,10 +17,12 @@ public class MultiIndexFactory {
 
     private final static BTreeIndexFactory<String, Integer> INTERNAL_INDEX_FACTORY = new BTreeIndexFactory<String, Integer>();
     //
+    private final Paged paged;
     private final SortedIndex<String, Integer> multiIndex;
 
     public MultiIndexFactory(Paged paged) {
-        multiIndex = INTERNAL_INDEX_FACTORY.openOrCreate(paged);
+        this.paged = paged;
+        this.multiIndex = INTERNAL_INDEX_FACTORY.openOrCreate(paged);
     }
 
     /**
@@ -28,12 +30,11 @@ public class MultiIndexFactory {
      * 
      * @param <Key> The index key type.
      * @param <Value> The index value type.
-     * @param paged The paged object used to store the index.
      * @param indexName The unique index name.
      * @param indexFactory The index factory used to actually create the index.
      * @return The newly created index.
      */
-    public <Key, Value> Index<Key, Value> create(Paged paged, String indexName, IndexFactory<Key, Value> indexFactory) {
+    public <Key, Value> Index<Key, Value> create(String indexName, IndexFactory<Key, Value> indexFactory) {
         if (!multiIndex.containsKey(indexName)) {
             Index<Key, Value> result = indexFactory.create(paged);
             multiIndex.put(indexName, result.getIndexLocation());
@@ -48,12 +49,11 @@ public class MultiIndexFactory {
      * 
      * @param <Key> The index key type.
      * @param <Value> The index value type.
-     * @param paged The paged object used to load the index.
      * @param indexName The unique index name.
      * @param indexFactory The index factory used to actually open the index.
      * @return The opened index.
      */
-    public <Key, Value> Index<Key, Value> open(Paged paged, String indexName, IndexFactory<Key, Value> indexFactory) {
+    public <Key, Value> Index<Key, Value> open(String indexName, IndexFactory<Key, Value> indexFactory) {
         Integer location = multiIndex.get(indexName);
         if (location != null) {
             Index<Key, Value> result = indexFactory.open(paged, location);
@@ -68,12 +68,11 @@ public class MultiIndexFactory {
      * 
      * @param <Key> The index key type.
      * @param <Value> The index value type.
-     * @param paged The paged object used to load/store the index.
      * @param indexName The unique index name.
      * @param indexFactory The index factory used to actually open or create the index.
      * @return The index.
      */
-    public <Key, Value> Index<Key, Value> openOrCreate(Paged paged, String indexName, IndexFactory<Key, Value> indexFactory) {
+    public <Key, Value> Index<Key, Value> openOrCreate(String indexName, IndexFactory<Key, Value> indexFactory) {
         Integer location = multiIndex.get(indexName);
         Index<Key, Value> result = null;
         if (location != null) {
--- src/test/java/org/fusesource/hawtdb/api/MultiIndexApiTest.java
+++ src/test/java/org/fusesource/hawtdb/api/MultiIndexApiTest.java
@@ -26,9 +26,9 @@ public class MultiIndexApiTest {
         IndexFactory<String, String> indexFactory1 = new BTreeIndexFactory<String, String>();
         IndexFactory<String, String> indexFactory2 = new HashIndexFactory<String, String>();
 
-        Index<String, String> index1 = multiIndexFactory.create(page, "1", indexFactory1);
-        Index<String, String> index2 = multiIndexFactory.create(page, "2", indexFactory2);
-        
+        Index<String, String> index1 = multiIndexFactory.create("1", indexFactory1);
+        Index<String, String> index2 = multiIndexFactory.create("2", indexFactory2);
+
         assertEquals(2, multiIndexFactory.indexes().size());
         assertTrue(multiIndexFactory.indexes().contains("1"));
         assertTrue(multiIndexFactory.indexes().contains("2"));
@@ -50,8 +50,8 @@ public class MultiIndexApiTest {
         indexFactory1 = new BTreeIndexFactory<String, String>();
         indexFactory2 = new HashIndexFactory<String, String>();
 
-        index1 = multiIndexFactory.open(page, "1", indexFactory1);
-        index2 = multiIndexFactory.open(page, "2", indexFactory2);
+        index1 = multiIndexFactory.open("1", indexFactory1);
+        index2 = multiIndexFactory.open("2", indexFactory2);
 
         assertEquals("b", index2.get(index1.get("1")));
 
@@ -61,7 +61,7 @@ public class MultiIndexApiTest {
 
         tmpFile.delete();
     }
-    
+
     @Test
     public void testOpenOrCreateIndex() throws IOException {
         File tmpFile = File.createTempFile("hawtdb", "test");
@@ -78,9 +78,9 @@ public class MultiIndexApiTest {
         IndexFactory<String, String> indexFactory1 = new BTreeIndexFactory<String, String>();
         IndexFactory<String, String> indexFactory2 = new HashIndexFactory<String, String>();
 
-        Index<String, String> index1 = multiIndexFactory.openOrCreate(page, "1", indexFactory1);
-        Index<String, String> index2 = multiIndexFactory.openOrCreate(page, "2", indexFactory2);
-        
+        Index<String, String> index1 = multiIndexFactory.openOrCreate("1", indexFactory1);
+        Index<String, String> index2 = multiIndexFactory.openOrCreate("2", indexFactory2);
+
         assertEquals(2, multiIndexFactory.indexes().size());
         assertTrue(multiIndexFactory.indexes().contains("1"));
         assertTrue(multiIndexFactory.indexes().contains("2"));
@@ -102,8 +102,8 @@ public class MultiIndexApiTest {
         indexFactory1 = new BTreeIndexFactory<String, String>();
         indexFactory2 = new HashIndexFactory<String, String>();
 
-        index1 = multiIndexFactory.openOrCreate(page, "1", indexFactory1);
-        index2 = multiIndexFactory.openOrCreate(page, "2", indexFactory2);
+        index1 = multiIndexFactory.openOrCreate("1", indexFactory1);
+        index2 = multiIndexFactory.openOrCreate("2", indexFactory2);
 
         assertEquals("b", index2.get(index1.get("1")));
 
@@ -113,4 +113,49 @@ public class MultiIndexApiTest {
 
         tmpFile.delete();
     }
+
+    @Test(expected = IllegalStateException.class)
+    public void testCannotCreateIndexWithSameName() throws IOException {
+        File tmpFile = File.createTempFile("hawtdb", "test");
+
+        PageFileFactory pageFactory = new PageFileFactory();
+        pageFactory.setFile(tmpFile);
+        pageFactory.open();
+
+        PageFile page = pageFactory.getPageFile();
+
+        try {
+            MultiIndexFactory multiIndexFactory = new MultiIndexFactory(page);
+            IndexFactory<String, String> indexFactory1 = new BTreeIndexFactory<String, String>();
+            IndexFactory<String, String> indexFactory2 = new HashIndexFactory<String, String>();
+            Index<String, String> index1 = multiIndexFactory.create("1", indexFactory1);
+            Index<String, String> index2 = multiIndexFactory.create("1", indexFactory2);
+            fail("Exception was expected!");
+        } finally {
+            pageFactory.close();
+            tmpFile.delete();
+        }
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testCannotOpenNotExistentIndex() throws IOException {
+        File tmpFile = File.createTempFile("hawtdb", "test");
+
+        PageFileFactory pageFactory = new PageFileFactory();
+        pageFactory.setFile(tmpFile);
+        pageFactory.open();
+
+        PageFile page = pageFactory.getPageFile();
+
+        try {
+            MultiIndexFactory multiIndexFactory = new MultiIndexFactory(page);
+            IndexFactory<String, String> indexFactory1 = new BTreeIndexFactory<String, String>();
+            Index<String, String> index1 = multiIndexFactory.open("1", indexFactory1);
+            fail("Exception was expected!");
+        } finally {
+            pageFactory.close();
+            tmpFile.delete();
+        }
+    }
+
 }
